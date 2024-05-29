6d5b7d76bae7f6bb8058f196b2dbaf5f6f4a537a
„michellesaric“ <msaric03@fesb.hr>
2024-05-29 14:31:36 +0200

    Added a header to the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import CreateRecipe from "./CreateRecipe";
 import CategoryList from "./CategoryList";
+import Home from "./pages/Home/Home";
+
 
 const App = () => {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/create-recipe">Create Recipe</Link>
-            </li>
-            <li>
-              <Link to="/categories">Show categories</Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
           <Route path="/create-recipe" element={<CreateRecipe />} />
           <Route path="/categories" element={<CategoryList />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/Home/Header/Header.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import HeaderBackground from "../../../assets/images/HeaderBackground";
+import MediumIcon from "../../../assets/icons/MediumIcon";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [className, setClassName] = useState("header__link-wrapper");
+
+  const handleOpen = () => {
+    setClassName((prevClassName) => "header__link-wrapper-mobile");
+  };
+
+  const handleClose = () => {
+    setClassName((prevClassName) => "header__link-wrapper");
+  };
+  return (
+    <header className="header">
+      <HeaderBackground className="header__background" />
+      <div className="header__wrapper">
+        <div className="header__link-logo-wrapper">
+          <Link to="/" className="header__logo">
+            recipix<span className="header__logo-dot">.</span>
+          </Link>
+          <div className="header__medium-icon" onClick={handleOpen}>
+            <MediumIcon />
+          </div>
+          <div className={className}>
+            <p className="header__link" onClick={handleClose}>
+              calorie calculator
+            </p>
+            <p className="header__link" onClick={handleClose}>
+              recipes
+            </p>
+            <p className="header__link" onClick={handleClose}>
+              placeholder
+            </p>
+          </div>
+        </div>
+        <h1 className="header__main-title">Living smart? Living healthy?</h1>
+        <p className="header__main-title-subtext">
+          Create your own healthy multi-course meal based on whatever
+          ingredients you like
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
