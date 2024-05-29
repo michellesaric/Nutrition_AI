cc0ddc91961beea59365d75577b615886c02c2cf
„michellesaric“ <msaric03@fesb.hr>
2024-05-29 11:14:45 +0200

    Added assets for the frontend

---
+++ frontend/assets/icons/Checkmark.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Checkmark = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20 6L9 17L4 12"
+        stroke="#575757"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Checkmark;
---
+++ frontend/assets/icons/MediumIcon.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const MediumIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 9H19"
+        stroke="#575757"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5 15H19"
+        stroke="#575757"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default MediumIcon;
